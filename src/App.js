f6183d77a7aa6204f77b02f483de179c2bb35767
Fred Wang <thefblue@gmail.com>
2024-04-08 07:35:40 -0400

    updated home page

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/letterboxd-redesign" element={<Home/>} />
           <Route path="/films" element={<Films/>} />
           <Route path="/lists" element={<Lists/>} />
           <Route path="/profile" element={<Profile/>} />
